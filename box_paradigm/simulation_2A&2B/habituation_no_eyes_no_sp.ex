1db5c40d51c15da1ebf72b0653775f09c92f4127
Deon T. Benton <37884959+dtbenton@users.noreply.github.com>
2021-05-23 20:12:20 -0400

    s

---
+++ box_paradigm/simulation_2A&2B/habituation_no_eyes_no_sp.ex
@@ -0,0 +1,257 @@
+defT:-
+name: HelpingEvents1
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HinderingEvents1
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HelpingEvents2
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HinderingEvents2
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HelpingEvents3
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HinderingEvents3
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HelpingEvents4
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
+name: HinderingEvents4
+I:	
+(Slot_A)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1)    
+ 1 0
+(Slot_B)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 0 1
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 0 1
+(Box_IN)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                            
+ 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 0 1
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 0 1
+(Box_OUT)                                                  
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+;
---
+++ box_paradigm/simulation_3/pretraining_interaction.ex
@@ -0,0 +1,217 @@
+defT:-
+name: ConcordantAgentPastConcordantInteraction1
+I:	
+(Slot_A)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 1 0 0
+T:	
+(Slot_A_OUT)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 1 0 0
+(Interaction_OUT)    
+ 0 1
+;
+name: ConcordantAgentPastNoActionNoInteraction1
+I:	
+(Slot_A)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 0 1
+;
+name: ConcordantAgentPastNoActionInteraction1
+I:	
+(Slot_A)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 1 0
+;
+name: DiscordantAgentPastDiscordantNoInteraction1
+I:	
+(Slot_A)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 1 0
+T:	
+(Slot_A_OUT)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 1 0
+(Interaction_OUT)    
+ 1 0
+;
+name: DiscordantAgentPastNoActionNoInteraction1
+I:	
+(Slot_A)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 0 1
+;
+name: DiscordantAgentPastNoActionInteraction1
+I:	
+(Slot_A)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 1 0
+;
+name: ConcordantAgentPastConcordantInteraction2
+I:	
+(Slot_A)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 1 0 0
+T:	
+(Slot_A_OUT)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 1 0 0
+(Interaction_OUT)    
+ 0 1
+;
+name: ConcordantAgentPastNoActionNoInteraction2
+I:	
+(Slot_A)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 0 1
+;
+name: ConcordantAgentPastNoActionInteraction2
+I:	
+(Slot_A)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 1 0
+;
+name: DiscordantAgentPastDiscordantNoInteraction2
+I:	
+(Slot_A)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 1 0
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 1 0
+(Interaction_OUT)    
+ 1 0
+;
+name: DiscordantAgentPastNoActionNoInteraction2
+I:	
+(Slot_A)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 0 1
+;
+name: DiscordantAgentPastNoActionInteraction2
+I:	
+(Slot_A)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 0 1
+T:	
+(Slot_A_OUT)                            
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Memory_OUT)      
+ 0 0 1
+(Interaction_OUT)    
+ 1 0
+;

eef1fa5077629547896abcab7b0e0b28b9061b77
Deon T. Benton <37884959+dtbenton@users.noreply.github.com>
2021-09-19 22:01:16 -0400

    s

---
+++ hamlin_wynn_bloom_mahajan2011/habituation_third_party_help_condition.ex
@@ -0,0 +1,129 @@
+defT:-
+name: HelpingEvents1
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 1 0
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 1 0
+(Ball_IN)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 1 0
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 1 0
+(Ball_OUT)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+;
+name: HinderingEvents1
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1)    
+ 1 0
+(Slot_B)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2)    
+ 1 0
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 1 0
+(Ball_IN)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_2_OUT)    
+ 1 0
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 1 0
+(Ball_OUT)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+;
+name: HelpingEvents2
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1)    
+ 1 0
+(Slot_B)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 1 0
+(Motion_IN)      
+ 1 0 0
+(Propel_IN)    
+ 1 0
+(Ball_IN)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 1 0
+(Motion_OUT)      
+ 1 0 0
+(Propel_OUT)    
+ 1 0
+(Ball_OUT)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+;
+name: HinderingEvents2
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1)    
+ 1 0
+(Slot_B)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2)    
+ 1 0
+(Motion_IN)      
+ 0 1 0
+(Propel_IN)    
+ 1 0
+(Ball_IN)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+(Eyes_1_OUT)    
+ 1 0
+(Slot_B_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_2_OUT)    
+ 1 0
+(Motion_OUT)      
+ 0 1 0
+(Propel_OUT)    
+ 1 0
+(Ball_OUT)                                                  
+ 0 0 1 0 0 0 1 1 1 0 0 1 1 1 0 0 1 1 1 0 0 0 1 0 0
+;
---
+++ hamlin_wynn_bloom_mahajan2011/test.ex
@@ -0,0 +1,73 @@
+defT:-
+name: Helper1
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Interaction_OUT)    
+ 1 0
+(Memory_OUT)      
+ 1 0 0
+;
+name: Hinderer1
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 1 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Interaction_OUT)    
+ 0 1
+(Memory_OUT)      
+ 0 1 0
+;
+name: Helper2
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 1 0 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Interaction_OUT)    
+ 1 0
+(Memory_OUT)      
+ 1 0 0
+;
+name: Hinderer2
+I:	
+(Slot_A)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1)    
+ 1 0
+(Memory_IN)      
+ 0 1 0
+T:	
+(Slot_A_OUT)                                
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0
+(Eyes_1_OUT)    
+ 1 0
+(Interaction_OUT)    
+ 0 1
+(Memory_OUT)      
+ 0 1 0
+;
